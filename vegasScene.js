833a3ad2484e832b1c7f851a452db8aea4d621f1
unknown <maurice13stu@gmail.com>
2025-05-26 07:45:31 -0700

    Improve mobile modal and play button; fix play button to go to game.html

--- vegasScene.js
+++ vegasScene.js
@@ -108,7 +108,7 @@ document.getElementById('play-button').addEventListener('click', () => {
     document.body.style.transition = "opacity 0.5s";
     document.body.style.opacity = 0;
     setTimeout(() => {
-        window.location.href = 'index.html';
+        window.location.href = 'game.html'; // <-- change here
     }, 500);
 });
 
