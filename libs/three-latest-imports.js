61f95e2da6f0bec2e93a0babdb3f456cd8ff0516
unknown <maurice13stu@gmail.com>
2025-08-10 15:46:15 -0700

    .

--- libs/three-latest-imports.js
+++ libs/three-latest-imports.js
@@ -1,5 +1,5 @@
 // Updated Three.js and GLTFLoader imports for latest npm version
-import * as THREE from 'three';
+import * as THREE from './three.module.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 // ...rest of your code...
